8a049219a8f0f0c5105f6b6bbb74a4ecf242c50b
dicaprion <batrshina.n@gmail.com>
2018-11-27 10:57:44 +0500

    buttons

--- src/main/java/TelegramBot/TGBot.java
+++ src/main/java/TelegramBot/TGBot.java
@@ -5,9 +5,12 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class TGBot extends TelegramLongPollingBot implements Runnable {
 
@@ -51,6 +54,19 @@ public class TGBot extends TelegramLongPollingBot implements Runnable {
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setText(text);
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+        ArrayList<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add("date");
+        keyboardFirstRow.add("joke");
+        keyboard.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
